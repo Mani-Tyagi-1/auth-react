5d3da0be60576b33767d6f16fd3d167da16266d0
ManiTyagi <manityagi1919@gmail.com>
2024-05-27 01:42:42 +0530

    added user and admin role

--- src/components/LoginRegister/Login.jsx
+++ src/components/LoginRegister/Login.jsx
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../firebaseconfig";
+import { auth, db } from "../../firebaseconfig";
+import { doc, getDoc } from 'firebase/firestore';
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const [role, setRole] = useState(null);
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
-      await signInWithEmailAndPassword(auth, email, password);
-      alert("Login Successful");
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      // fetch user data from firebase
+      const userDoc = await getDoc(doc(db, "users", user.uid));
+      const adminDoc = await getDoc(doc(db, "admin", user.uid));
+      if (userDoc.exists()) { 
+        setRole(userDoc.data().role);
+        alert("Login Successful as user");
+      }
+      else if(adminDoc.exists()) {
+        const orgDoc = await getDoc(doc(db, "admin", user.uid));
+        if (orgDoc.exists()) { 
+          setRole(orgDoc.data().role);
+          alert("Login Successful as admin");
+        }
+      }
+      else {
+        alert("User not found... Please register first");
+      }
+      
     } catch (error) {
       setError(error.message);
     }
@@ -25,8 +45,8 @@ const Login = () => {
           <h1>Login</h1>
           <div>
             <input
-              type="text"
-              placeholder="username"
+              type="email"
+              placeholder="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -53,6 +73,7 @@ const Login = () => {
         </form>
 
         {error && <p>{error}</p>}
+        {!error &&role && <p>Logged in as : {role}</p>}
       </div>
     </div>
   );
--- src/components/LoginRegister/Register.jsx
+++ src/components/LoginRegister/Register.jsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../firebaseconfig";
+import { auth, db } from "../../firebaseconfig";
+import { doc, setDoc } from 'firebase/firestore';
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
   const [error, setError] = useState(null);
 
   const handleSignup = async (e) => {
     e.preventDefault();
+    if (!role) {
+      setError("Please select a role");
+      return;
+    }
 
     try {
-      await createUserWithEmailAndPassword(auth,email, password);
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      
+      const user = userCredential.user;
+      await setDoc(doc(db,role ==='user' ? "users" : "admin", user.uid), {
+        email,role,
+      })
       alert("Registration Successful");
     } catch (error) {
       setError(error.message);
@@ -40,6 +51,27 @@ const Register = () => {
               required
               onChange={(e) => setPassword(e.target.value)}
             />
+          </div>
+
+          <div>
+            <label>
+              <input
+                type="radio"
+                value="user"
+                checked={role === "user"}
+                onChange={() => setRole("user")}
+              />
+              Signup as User
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="organization"
+                checked={role === "organization"}
+                onChange={() => setRole("organization")}
+              />
+              Signup as Organization
+            </label>
           </div>
 
           <button type="submit">Register</button>
